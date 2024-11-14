9fb5ee6fd611732cbbc69101ba231a1cdf8a72db
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 18:38:59 -0300

    java introduction

--- src/main/java/com/matheus/training_java/UseIfs.java
+++ src/main/java/com/matheus/training_java/UseIfs.java
@@ -3,6 +3,14 @@ package com.matheus.training_java;
 public class UseIfs {
 
     public static void main(String[] args) {
+        var idade = 20;
 
+        if (idade > 21){
+            System.out.println("maior");
+        } else if (idade == 21) {
+            System.out.println("igual");
+        }else {
+            System.out.println("menor");
+        }
     }
 }
