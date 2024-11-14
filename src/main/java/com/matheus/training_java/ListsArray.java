505904e28d2a31a79b104dfd7619325544db9538
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 19:04:56 -0300

    java introduction

---
+++ src/main/java/com/matheus/training_java/ListsArray.java
@@ -0,0 +1,18 @@
+package com.matheus.training_java;
+import java.util.ArrayList;
+
+public class ListsArray {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> idades = new ArrayList<Integer>();
+        idades.add(20);
+        idades.add(30);
+        idades.add(40);
+        idades.add(50);
+        idades.remove(0);
+        idades.remove(2);
+        System.out.println(idades);
+
+
+    }
+}
