1306c265afa6835d9a4323c07097bfc8bbde1a55
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 18:06:37 -0300

    java introduction

---
+++ src/main/java/com/matheus/training_java/test.java
@@ -0,0 +1,20 @@
+package com.matheus.training_java;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        byte idade = 19;
+        short salario;
+
+        boolean brasileiro = true;
+
+        String nome ="Matheus";
+
+        double troco = 22.50;
+        float ganhos = 200.00F;
+
+
+
+    }
+}
