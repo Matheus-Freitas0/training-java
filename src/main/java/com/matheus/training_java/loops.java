06202809545f307e150308e510463c29bae0c0d7
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 19:16:44 -0300

    java introduction

---
+++ src/main/java/com/matheus/training_java/loops.java
@@ -0,0 +1,12 @@
+package com.matheus.training_java;
+
+public class loops {
+    public static void main(String[] args) {
+        int idade = 10;
+        int i;
+        for (i = 0; i < idade; i++) {
+            System.out.println(i);
+            System.out.println("oi");
+        }
+    }
+}
