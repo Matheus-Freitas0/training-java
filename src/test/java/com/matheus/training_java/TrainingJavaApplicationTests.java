84469cc6e2229785726ab44e34f7d3eee47293b7
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 13:47:31 -0300

    test commit

---
+++ src/test/java/com/matheus/training_java/TrainingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.training_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
