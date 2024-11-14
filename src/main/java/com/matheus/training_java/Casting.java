4c17e9df2ec169c4abf0d170a58f4ed83cd522ab
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-14 19:29:23 -0300

    java introduction

---
+++ src/main/java/com/matheus/training_java/Casting.java
@@ -0,0 +1,16 @@
+package com.matheus.training_java;
+
+public class Casting {
+    public static void main(String[] args) {
+        int idade1 = 22;
+        double idade2 = idade1;
+
+        idade1 = (int) idade2;
+
+        char letra = 'a';
+        String nome = String.valueOf(letra);
+        letra = nome.charAt(0);
+        String nome2 = String.valueOf(idade1);
+        idade1 = Integer.parseInt(nome2);
+    }
+}
