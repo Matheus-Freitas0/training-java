b489400c7f2cfc7f025b1169ae7364978de946ac
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-15 17:09:00 -0300

    POO started

---
+++ src/main/java/com/matheus/training_java/POO.java
@@ -0,0 +1,21 @@
+package com.matheus.training_java;
+
+public class POO {
+    String nome;
+
+    static String nome2;
+
+    public static void main(String[] args) {
+        System.out.println(nome2);
+    }
+
+    void declaraNome(){
+        nome = "Matheus";
+        System.out.println(nome2);
+
+    }
+
+    String getNome(){
+        return nome;
+    }
+}
