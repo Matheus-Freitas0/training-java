92e126bbb6345241f4eca82feb29423caa318dc1
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-19 11:07:50 -0300

    date updated

--- src/main/java/com/matheus/training_java/date/RangeDate.java
+++ src/main/java/com/matheus/training_java/date/RangeDate.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 public class RangeDate {
 
     public static void main(String[] args) {
+
         LocalDate inicio = LocalDate.of(2024, 11, 10);
         LocalDate fim = LocalDate.of(2024, 11, 20);
         LocalDate dataParaComparar = LocalDate.of(2024, 11, 15);
