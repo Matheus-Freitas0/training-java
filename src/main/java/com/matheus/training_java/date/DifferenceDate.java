53f2cba6d68f0b011f40b7ab7387e80671019fe6
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-19 11:38:47 -0300

    difference date implemented

---
+++ src/main/java/com/matheus/training_java/date/DifferenceDate.java
@@ -0,0 +1,13 @@
+package com.matheus.training_java.date;
+
+public class DifferenceDate {
+
+    public static void main(String[] args) {
+        LocalDate data1 = LocalDate.of(2024, 11, 10);
+        LocalDate data2 = LocalDate.of(2025, 11, 10);
+
+        Period periodo = Period.between(data1, data2);
+        System.out.println("Diferença: " + periodo.getYears() + " anos, " + periodo.getMonths() + " meses, " + periodo.getDays() + " dias.");
+    }
+
+}
